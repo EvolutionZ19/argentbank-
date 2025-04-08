4c0d81202de5220db966e5515cd068b756d21161
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2025-04-08 10:08:17 +0200

    Add Home component and update App.jsx

---
+++ argent-bank/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h1>Bienvenue sur Argent Bank</h1>
+        <p>Page d'accueil en construction...</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
