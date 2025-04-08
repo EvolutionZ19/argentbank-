2df37592d435af98f594111111040b70f1111f87
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2025-04-08 15:31:51 +0200

    feat(Profile): enhance profile page layout and redirect to login if user is not authenticated

--- frontEnd/src/pages/Profile/Profile.jsx
+++ frontEnd/src/pages/Profile/Profile.jsx
@@ -3,22 +3,63 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function Profile() {
+  // Récupère le prénom de l'utilisateur depuis le store Redux
   const firstName = useSelector((state) => state.user.firstName);
   const navigate = useNavigate();
 
   useEffect(() => {
+    // Si aucun prénom, redirige vers /login
     if (!firstName) {
-      // Si l'utilisateur actualise la page sans passer par /login
       navigate("/login");
     }
   }, [firstName, navigate]);
 
   return (
     <main className="profile-page">
-      <section>
-        <h1>Bienvenue sur votre profil</h1>
-        <p>👋 Bonjour, {firstName} !</p>
-        <p>Cette page affichera vos informations personnelles.</p>
+      {/* En-tête de la page avec message de bienvenue et bouton de modification */}
+      <div className="header">
+        <h1>
+          Welcome back
+          <br />
+          {firstName}!
+        </h1>
+        <button>Edit Name</button>
+      </div>
+
+      {/* Liste des comptes affichés */}
+      <h2 className="sr-only">Accounts</h2>
+
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+          <p className="account-amount">$2,082.79</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+          <p className="account-amount">$10,928.42</p>
+          <p className="account-amount-description">Available Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+          <p className="account-amount">$184.30</p>
+          <p className="account-amount-description">Current Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
       </section>
     </main>
   );
