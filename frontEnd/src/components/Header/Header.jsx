a9ba26e67c7c6568b66ec7b92af1c4b5884c8420
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2025-04-08 14:14:32 +0200

    feat(store): add Redux store and user reducer for managing user state
    - Created a Redux store in `index.js` with a user reducer.
    - Implemented user slice in `userReducer.js` to manage user first name.
    - Added action `setFirstName` to update the user's first name in the state.

--- frontEnd/src/components/Header/Header.jsx
+++ frontEnd/src/components/Header/Header.jsx
@@ -1,11 +1,77 @@
-function Header() {
-    return (
-      <header>
-        <h1>Argent Bank</h1>
-        {/*menu de navigation*/}
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import argentBankLogo from '../../assets/logo/argentBankLogo.png';
+
+
+/**
+ * Header Component
+ * @returns {JSX.Element} Header Component
+ */
+export default function Header() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+  const isUserPage = location.pathname === '/profile';
+  const isSignInPage = location.pathname === '/login';
+  const isTransactionsPage = location.pathname === '/transactions';
+
+  const getfirstName = useSelector((state) => state.user.firstName);
+
+  return (
+    <header>
+      <nav className='main-nav'>
+        {isHomePage || isSignInPage ? (
+          <>
+            <a className='main-nav-logo' href='/'>
+              <img
+                className='main-nav-logo-image'
+                alt='Argent Bank Logo'
+                src={argentBankLogo}
+              />
+              <h1 className='sr-only'>Argent Bank</h1>
+            </a>
+            <div>
+              <a className='main-nav-item' href='/login'>
+                <i className='fa fa-user-circle'></i>
+                Sign In
+              </a>
+            </div>
+          </>
+        ) : null}
+        {isUserPage ? (
+          <>
+            <a className='main-nav-logo' href='/'>
+              <img
+                className='main-nav-logo-image'
+                alt='Argent Bank Logo'
+                src={argentBankLogo}
+              />
+              <h1 className='sr-only'>Argent Bank</h1>
+            </a>
+            <div>
+              <a className='main-nav-item'>
+                <i className='fa fa-user-circle'></i>
+                {getfirstName}
+              </a>
+              <a className='main-nav-item' href='/'>
+                <i className='fa fa-sign-out'></i>
+                Sign Out
+              </a>
+            </div>
+          </>
+        ) : null}
+        {isTransactionsPage ? (
+          <>
+            <a className='main-nav-logo' href='/'>
+              <img
+                className='main-nav-logo-image'
+                alt='Argent Bank Logo'
+                src={argentBankLogo}
+              />
+              <h1 className='sr-only'>Argent Bank</h1>
+            </a>
+          </>
+        ) : null}
+      </nav>
+    </header>
+  );
+}
