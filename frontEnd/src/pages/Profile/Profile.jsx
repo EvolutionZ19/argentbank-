ecc8026220517fb4c2e6e837c516b7b931989944
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2025-04-09 12:19:21 +0200

    feat: consolidate CSS styles into a single stylesheet and remove unused CSS files

--- frontEnd/src/pages/Profile/Profile.jsx
+++ frontEnd/src/pages/Profile/Profile.jsx
@@ -1,4 +1,3 @@
-import "./Profile.css";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
--- frontEnd/src/pages/Transactions/Transactions.jsx
+++ frontEnd/src/pages/Transactions/Transactions.jsx
@@ -1,4 +1,3 @@
-import "./Transactions.css";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
