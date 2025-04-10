ce1369f60f2cdd34a4f01f072a037e1aa53eb07b
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2025-04-10 13:57:33 +0200

    feat(Redux): implement user slice with authentication and profile management

---
+++ frontEnd/src/redux/slice/rootReducer.js
@@ -0,0 +1,9 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './slice/rootReducer';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
--- frontEnd/src/redux/store/userReducer.js
+++ frontEnd/src/redux/store/userReducer.js
@@ -1,18 +1,55 @@
+// src/store/userSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  firstName: null,
+  id: null,
+  email: '',
+  firstName: '',
+  lastName: '',
+  token: undefined,
+  error: false,
+  errorMessage: '',
+  isConnected: false,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setFirstName: (state, action) => {
-      state.firstName = action.payload;
+    signIn: (state, action) => {
+      state.token = action.payload.token;
+      state.isConnected = true;
+      state.error = false;
+    },
+    signUp: (state, action) => {
+      state.token = action.payload.token;
+      state.error = false;
+    },
+    logOut: () => initialState,
+    getProfile: (state, action) => {
+      state.email = action.payload.email;
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
+      state.id = action.payload.id;
+    },
+    editProfile: (state, action) => {
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
+    },
+    showError: (state, action) => {
+      state.error = true;
+      state.errorMessage = action.payload.errorMessage;
     },
   },
 });
 
-export const { setFirstName } = userSlice.actions;
+export const {
+  signIn,
+  signUp,
+  logOut,
+  getProfile,
+  editProfile,
+  showError,
+} = userSlice.actions;
+
 export default userSlice.reducer;
